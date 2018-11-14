3b857c95fcb15858b3459fc875cb5f5521fbead4
vincent <j1761@matsue-ct.jp>
2018-11-14 10:44:44 +0900

    fizzbuzz program

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+for num in 0..100
+	if (num % 3 == 0) && (num % 5 == 0) then
+		puts "FizzBuzz"
+	elsif (num % 3 == 0) then
+		puts "Fizz"
+	elsif (num % 5 == 0) then
+		puts "Buzz"
+	else
+		puts num
+	end
+end
+
